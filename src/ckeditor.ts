6805d851c29d569cc85644a6be87ce5e9d4808e4
Rajat Tyagi <tyagirajat200@gmail.com>
2024-03-20 12:06:08 +0530

    Auto-commit before publishing

--- src/ckeditor.ts
+++ src/ckeditor.ts
@@ -36,10 +36,13 @@ import { EMOJIS_ARRAY, LANGUAGES, REDUCED_MATERIAL_COLORS } from "./data";
 import { Locale } from '@ckeditor/ckeditor5-utils';
 
 import { createLabeledInputText, LabeledFieldView } from '@ckeditor/ckeditor5-ui';
-
+import { Template, View } from '@ckeditor/ckeditor5-ui';
 
 import './styles.css'
 import './content.css'
+
+
+const isNil = (value: number) => value == null || value === undefined;
 
 /**
  * Enrich the special characters plugin with emojis.
@@ -87,8 +90,6 @@ class CodeBlockSearchPlugin extends Plugin {
 					})
 				};
 			});
-			if (this.editor.plugins.get('WordCount')) { this.editor.ui.element?.querySelector('.ck.ck-editor__main')?.appendChild(this.editor.plugins.get('WordCount').wordCountContainer); }
-
 		})
 	}
 
@@ -114,6 +115,90 @@ class CodeBlockSearchPlugin extends Plugin {
 				resolve(element);
 			}
 		});
+	}
+}
+
+
+class WordCountPlugin extends WordCount {
+	private outputView: View | undefined;
+
+	constructor(editor: Editor) {
+		super(editor)
+	}
+
+	init(): void {
+		super.init();
+		this.listenTo(this.editor, 'ready', () => {
+			if (this.editor.plugins.get('WordCount')) {
+				this.editor.ui.element?.querySelector('.ck.ck-editor__main')?.appendChild(this.wordCountContainer);
+			}
+		})
+	}
+
+	public get wordCountContainer(): HTMLElement {
+		this
+		const editor = this.editor;
+		const t = editor.t;
+		const displayWords = editor.config.get('wordCount.displayWords');
+		const displayCharacters = editor.config.get('wordCount.displayCharacters');
+		const maxWords = editor.config.get('wordCount.maxWords') as number;
+		const maxCharacters = editor.config.get('wordCount.maxCharacters') as number;
+		const bind = Template.bind(this, this);
+		const children = [];
+
+		if (!this.outputView) {
+			this.outputView = new View();
+			if (displayWords || displayWords === undefined) {
+				this.bind('_wordsLabel').to(this, 'words', words => t(`Words: %0${isNil(maxWords) ? '' : `/${maxWords}`}`, words));
+
+				children.push({
+					tag: 'div',
+					children: [
+						{
+							text: [bind.to('_wordsLabel')]
+						}
+					],
+					attributes: {
+						class: 'ck-word-count__words'
+					}
+				});
+			}
+
+			if (displayCharacters || displayCharacters === undefined) {
+				this.bind('_charactersLabel').to(this, 'characters', words => t(`Characters: %0${isNil(maxCharacters) ? '' : `/${maxCharacters}`}`, words));
+
+				children.push({
+					tag: 'div',
+					children: [
+						{
+							text: [bind.to('_charactersLabel')]
+						}
+					],
+					attributes: {
+						class: 'ck-word-count__characters'
+					}
+				});
+			}
+
+			this.outputView.setTemplate({
+				tag: 'div',
+				attributes: {
+					class: [
+						'ck',
+						'ck-word-count'
+					]
+				},
+				children
+			});
+
+			this.outputView.render();
+		}
+
+		return this.outputView.element!;
+	}
+
+	destroy(): void {
+		this.stopListening();
 	}
 }
 
@@ -180,7 +265,7 @@ ClassicEditor.builtinPlugins = [
 	TextTransformation,
 	TodoList,
 	Underline,
-	WordCount,
+	WordCountPlugin
 ];
 ClassicEditor.defaultConfig = {
 	toolbar: {
